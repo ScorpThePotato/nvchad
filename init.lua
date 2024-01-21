58432adc6357441d35bb861f473093af3301a593
Scorp <34991644+ScorpThePotato@users.noreply.github.com>
2024-01-21 19:19:22 +0200

    Added config options for Neovide

--- init.lua
+++ init.lua
@@ -5,3 +5,17 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+--
+
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  vim.o.guifont = "JetBrainsMono Nerd Font:h10" -- text below applies for VimScript
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_cursor_antialiasing = true
+  vim.g.neovide_cursor_animate_command_line = true
+  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+  vim.g.neovide_cursor_vfx_particle_density = 11.0
+  vim.opt.title = true
+  vim.opt.titlelen = 0 -- do not shorten title
+  vim.opt.titlestring = 'nvim %{expand("%:p")}'
+end
